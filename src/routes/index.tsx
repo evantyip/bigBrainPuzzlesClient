1c91705f231f91e42acb9b45a41005ac74120b82
Evan Yip <evanyip@Evans-Air.attlocal.net>
2025-04-09 20:46:10 -0700

    Initial Commit

---
+++ src/routes/index.tsx
@@ -0,0 +1,40 @@
+import { createFileRoute, Link } from '@tanstack/react-router'
+import { Route as puzzleRoute } from './puzzles'
+import logo from '../logo.svg'
+import '../App.css'
+
+export const Route = createFileRoute('/')({
+  component: App,
+})
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/routes/index.tsx</code> and save to reload.
+        </p>
+        <p>
+          Hello world
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+        <a
+          className="App-link"
+          href="https://tanstack.com"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn TanStack
+        </a>
+      </header>
+    </div>
+  )
+}
